9f09c4d77f074192f60560af7be6ad7245bdd540
Tomi Akin <tomia01@hotmail.com>
2024-07-24 20:14:11 +0000

    add url pattern for post detail function

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('',views.PostList.as_view(),name = 'bloglist'),
-    path('share_post',views.SharePost.as_view(),name = 'share_post')
+    path('share_post',views.SharePost.as_view(),name = 'share_post'),
+    path('<slug:slug>/', views.post_detail, name='post_detail')
 ]
