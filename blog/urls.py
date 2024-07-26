754c6ff05d60ffe8092d6c7506c7e3037ee89122
Tomi Akin <tomia01@hotmail.com>
2024-07-26 12:46:03 +0000

    add url for post like function

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('share_post',views.SharePost.as_view(),name = 'share_post'),
     path('<int:post_id>/', views.post_detail, name='post_detail'),
     path('edit_post/<int:pk>', views.EditPost.as_view(), name='edit_post'),
+    path('post/<int:post_id>/like/',PostLike.as_view(),name='post_like')
 ]
